7b5349e4dadcff54a3882919144567de264444a8
FrankYeah <aaa24295234@gmail.com>
2021-11-29 01:29:02 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	// const swiper1 = new Swiper(".swiper-container1", {
+	// 	loop: true,
+	// 	lazy: true,
+	// 	autoplay: {
+	// 		delay: 3000
+	// 	},
+	// 	slidesPerView: 1,
+	// 	on: {
+	// 		slideChange: function () {
+	// 			const index_currentSlide = this.realIndex;
+	// 			const currentSlide = this.slides[index_currentSlide]
+	// 			console.log(index_currentSlide)
+	// 			console.log(currentSlide)
+	// 		},
+	// 	},
+	// })
+
+	
+})
